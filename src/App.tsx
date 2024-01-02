66b5ad866eba695f5eecf4ca0e901586b4f80efc
Douglas Souza <lyricsan@hotmail.com>
2024-01-02 14:35:30 -0300

    delete unnecessary page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
-import BookDetailPage from "./pages/BookDetailPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SearchPage />} />
-        <Route path="/book/:id" element={<BookDetailPage />} />
       </Routes>
     </Router>
   );
